d367712afe13510e778564fd590260f065111904
Mahendra890() <130951660+zzasaa-jpg@users.noreply.github.com>
2025-09-26 17:45:10 +0530

    Update n_move_pattern_obj.js
    Incorrect index corrected.

--- n_move_pattern_obj.js
+++ n_move_pattern_obj.js
@@ -81,7 +81,7 @@ export default function n_values_move_pattern(array) {
 			33:[array[4][0], array[4][1], array[4][2], array[4][3], array[4][4], array[4][6], array[0][5], array[1][5],array[2][5], array[3][5], array[5][5], array[6][5]],
 			34:[array[4][0], array[4][1], array[4][2], array[4][3], array[4][4], array[4][5], array[0][6], array[1][6],array[2][6], array[3][6], array[5][6], array[6][6]],
 			35:[array[5][1], array[5][2], array[5][3], array[5][4], array[5][5], array[5][6], array[0][0], array[1][0],array[2][0], array[3][0], array[4][0], array[6][0]],
-			36:[array[5][0], array[5][2], array[5][3], array[5][4], array[5][5], array[5][6], array[0][1], array[1][1],array[2][0], array[3][1], array[4][1], array[6][1]],
+			36:[array[5][0], array[5][2], array[5][3], array[5][4], array[5][5], array[5][6], array[0][1], array[1][1],array[2][1], array[3][1], array[4][1], array[6][1]],
 			37:[array[5][0], array[5][1], array[5][3], array[5][4], array[5][5], array[5][6], array[0][2], array[1][2],array[2][2], array[3][2], array[4][2], array[6][2]],
 			38:[array[5][0], array[5][1], array[5][2], array[5][4], array[5][5], array[5][6], array[0][3], array[1][3],array[2][3], array[3][3], array[4][3], array[6][3]],
 			39:[array[5][0], array[5][1], array[5][2], array[5][3], array[5][5], array[5][6], array[0][4], array[1][4],array[2][4], array[3][4], array[4][4], array[6][4]],
